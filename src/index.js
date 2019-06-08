35cb738112344e0bff5ffd9c1b1d06db9d4c8dae
Preston Elliott <preston.elliott24@gmail.com>
2019-06-08 12:01:48 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  form = document.getElementById("create-task-form")
+  input = document.querySelector("#new-task-description")
+  form.addEventListener("submit", createTask)
+})
+
+function createTask(e) {
+  e.preventDefault()
+  const newTask = document.createElement('li')
+  newTask.innerHTML = input.value
+  
+  addTask(newTask)
+  e.target.reset()
+}
+
+function addTask(task) {
+  document.querySelector("#tasks").appendChild(task)
+}
